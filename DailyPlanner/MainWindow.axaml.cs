24db042d90064161927780ccd76ef1f1af88e70e
ignacy <adsa@asda.com>
2025-03-11 15:51:37 +0100

    add Zadanie class

--- DailyPlanner/MainWindow.axaml.cs
+++ DailyPlanner/MainWindow.axaml.cs
@@ -2,6 +2,16 @@ using Avalonia.Controls;
 
 namespace DailyPlanner;
 
+class Zadanie
+{
+    public Zadanie()
+    {
+        string Nazwa;
+        string Kategoria;
+        bool CzyUkonczone;
+    }
+}
+
 public partial class MainWindow : Window
 {
     public MainWindow()
